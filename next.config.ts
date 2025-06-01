301ced065a31c7ca9a655c75330bc6b2d771dced
Sheikh763 <sheik32468@gmail.com>
2025-06-01 20:11:37 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',             // Enables static export
-  basePath: '/portfolio',       // IMPORTANT for GitHub Pages path
+  output: 'export',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   images: {
-    unoptimized: true,          // Required if using next/image
+    unoptimized: true,
   },
-  trailingSlash: true           // Required to make all routes work on GitHub Pages
 };
 
 module.exports = nextConfig;
