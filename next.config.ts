57172b0aa74a3edd1a143a623c8ada53a5ba4ae3
Sheikh763 <sheik32468@gmail.com>
2025-06-01 20:39:15 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
+  output: 'export', // required for static export
+  basePath: '/portfolio', // required for GitHub Pages subfolder
   images: {
-    unoptimized: true,
+    unoptimized: true, // avoids Image Optimization issues
   },
 };
 
